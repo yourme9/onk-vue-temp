0410377ac198e78382808fabcf5c9c0a44273296
OneWord <1498447015@qq.com>
2021-02-09 19:49:37 +0800

    初始化页面

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Onk脚手架内置封装axios网络模块，更多丰富内容敬请期待...</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+h3{
+  color: #ccc;
+}
+</style>
